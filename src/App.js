641d8697dd903ea367c26bbb52eec223d5db05f2
PhanBaoThanh <thanhninjahero@gmail.com>
2022-10-07 11:41:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {useState} from 'react'
 
 function App() {
+  const [counter1,setCounter1] = useState(0)
+  const [counter2,setCounter2] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='counter'>
+        <h2 className='counter__header'>First Counter</h2>
+        <p className='counter__value' style={{color: `${counter1 > 0 ? 'green' : counter1 < 0 ? 'red' : 'black'}`}}>{counter1}</p>
+        <div className='btns'>
+          <div className='btn' onClick={() => setCounter1(prev => prev - 1)}>DECREASE</div>
+          <div className='btn' onClick={() => setCounter1(0)}>RESET</div>
+          <div className='btn' onClick={() => setCounter1(prev => prev + 1)}>INCREASE</div>
+        </div>
+      </div>
+      <div className='counter'>
+        <h2 className='counter__header'>Second Counter</h2>
+        <p className='counter__value' style={{color: `${counter2 > 0 ? 'green' : counter2 < 0 ? 'red' : 'black'}`}}>{counter2}</p>
+        <div className='btns'>
+          <div className='btn' onClick={() => setCounter2(prev => prev - 1)}>DECREASE</div>
+          <div className='btn' onClick={() => setCounter2(0)}>RESET</div>
+          <div className='btn' onClick={() => setCounter2(prev => prev + 1)}>INCREASE</div>
+        </div>
+      </div>
     </div>
   );
 }
